dbe89cb935a9ff43b72c80aed796a00c095a258a
tiya adugna <tiyakefalee@gmail.com>
2020-04-04 01:35:10 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_concat(arr1, arr2)
+  arr1.concat(arr2)
+end
+def using_insert(array, element)
+  array.insert(4,element)
+end
+def using_uniq(array)
+  array.uniq
+end
+def using_flatten(array)
+  array.flatten 
+end
+def using_delete(array, s)
+  array.delete(s)
+end
+def using_delete_at(array, i)
+  array.delete_at(i)
+end
